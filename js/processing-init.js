5b76777286a592a407e3f390d293b7444cd469ae
norpol <t@woerm.at>
2014-05-19 10:30:59 +0200

    Moved jade files to one directory, removed contents of js/processing-init.js

--- js/processing-init.js
+++ js/processing-init.js
@@ -1,15 +1,1 @@
-console.log("Happy penguins");
-void setup()
-{
-    size(1024,768);
-    background(75);
-    fill(255);
-    noLoop();
-    PFont fontA = loadFont("courier");
-    textFont(fontA, 14);
-}
 
-void draw(){
-    text("Hello node-webkit",20,20);
-    printIn("Hello ErrorLog!");
-}
