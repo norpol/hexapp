229dd176d6713b13764d0437943949ab95384b49
norpol <t@woerm.at>
2014-05-19 10:22:25 +0200

    updated processing code, console.log not possible?

--- js/processing-init.js
+++ js/processing-init.js
@@ -1,40 +1,15 @@
-
-// Simple way to attach js code to the canvas is by using a function
-function sketchProc(processing) {
-  // Override draw function, by default it will be called 60 times per second
-  processing.draw = function() {
-    // determine center and max clock arm length
-    var centerX = processing.width / 2, centerY = processing.height / 2;
-    var maxArmLength = Math.min(centerX, centerY);
-
-    function drawArm(position, lengthScale, weight) {      
-      processing.strokeWeight(weight);
-      processing.line(centerX, centerY, 
-        centerX + Math.sin(position * 2 * Math.PI) * lengthScale * maxArmLength,
-        centerY - Math.cos(position * 2 * Math.PI) * lengthScale * maxArmLength);
-    }
-
-    // erase background
-    processing.background(224);
-
-    var now = new Date();
-
-    // Moving hours arm by small increments
-    var hoursPosition = (now.getHours() % 12 + now.getMinutes() / 60) / 12;
-    drawArm(hoursPosition, 0.5, 5);
-
-    // Moving minutes arm by small increments
-    var minutesPosition = (now.getMinutes() + now.getSeconds() / 60) / 60;
-    drawArm(minutesPosition, 0.80, 3);
-
-    // Moving hour arm by second increments
-    var secondsPosition = now.getSeconds() / 60;
-    drawArm(secondsPosition, 0.90, 1);
-  };
-  
+console.log("Happy penguins");
+void setup()
+{
+    size(1024,768);
+    background(75);
+    fill(255);
+    noLoop();
+    PFont fontA = loadFont("courier");
+    textFont(fontA, 14);
 }
 
-var canvas = document.getElementById("canvas1");
-// attaching the sketchProc function to the canvas
-var p = new Processing(canvas, sketchProc);
-// p.exit(); to detach it
+void draw(){
+    text("Hello node-webkit",20,20);
+    printIn("Hello ErrorLog!");
+}
